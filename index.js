3e93dbb403e3e5e90ef7b60a74d0961b25c9d358
Your Name <you@example.com>
2024-05-05 15:59:40 +0800

    commit changes

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 const port = 9000;
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const multer = require('multer'); 
-const { createReadStream } = require('fs');
-const qrCodeReader = require('qrcode-reader'); // library for decoding QR codes
-const Jimp = require('jimp'); 
+const multer = require("multer");
+const { createReadStream } = require("fs");
+const qrCodeReader = require("qrcode-reader"); // library for decoding QR codes
+const Jimp = require("jimp");
+
+const User = require("./models/user");
+const Enroll = require("./models/enrolled");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,31 +21,62 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('qrImage'), async (req, res) => {
+app.post("/upload", upload.single("qrImage"), async (req, res) => {
   try {
+    const username = req.cookies.username;
     if (!req.file) {
-      return res.status(400).send('No file uploaded');
+      return res.status(400).send("No file uploaded");
     }
 
     // Load the uploaded image using Jimp
     const image = await Jimp.read(req.file.buffer);
-    console.log(image)
+    // console.log(image)
 
     // Decode QR code from the image
-    const qr = new qrCodeReader();
-    qr.callback = (err, value) => {
-      if (err) {
-        return res.status(400).send('Error decoding QR code');
-      }
-      res.send({
-        message: 'QR code decoded successfully',
-        data: value.result,
-      });
-    };
-    qr.decode(image.bitmap);
+    const value = await new Promise((resolve, reject) => {
+      const qr = new qrCodeReader();
+      qr.callback = (err, value) => {
+        if (err) {
+          reject("Error decoding QR code");
+        } else {
+          resolve(value);
+        }
+      };
+      qr.decode(image.bitmap);
+    });
+
+    console.log(value.result);
+
+    const findUser = await User.findAll({
+      where: {
+        username: username,
+        user_type: "Student",
+      },
+      attributes: ["username", "code"],
+    });
+
+    const checkifEnrolled = await Enroll.findOne({
+      where: {
+        username: username,
+        code: value.result,
+      },
+    });
+    if (checkifEnrolled) {
+      return res.status(400).json({ message: "Already Enrolled" });
+    }
+    await Enroll.create({
+      username: username,
+      student_code: findUser.code,
+      code: value.result,
+    });
+
+    res.send({
+      message: "QR code decoded successfully",
+      data: value.result,
+    });
   } catch (error) {
-    console.error('Error processing image:', error);
-    return res.status(500).send('Internal Server Error');
+    console.error("Error processing image:", error);
+    return res.status(500).send("Internal Server Error");
   }
 });
 
